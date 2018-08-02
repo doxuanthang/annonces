f0e92aefa71770699773b0ad5af1c73823d2b817
Đỗ Xuân Thắng <doxuanthang@hotmail.com>
2018-08-02 16:56:46 +0700

    Update

--- annonces/pipelines.py
+++ annonces/pipelines.py
@@ -4,8 +4,18 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from pymongo import MongoClient
 
 class AnnoncesPipeline(object):
+
+    def __init__(self):
+        self.client = MongoClient('localhost', 27017)
+        self.db = self.client['scrapy']
+        self.collection = self.db['test-collection']
+
     def process_item(self, item, spider):
+        try:
+            self.collection.insert_one(dict(item))
+        except:
+            pass
         return item
